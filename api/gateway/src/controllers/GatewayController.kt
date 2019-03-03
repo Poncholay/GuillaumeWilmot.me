0c9cf4f5fc98b046d9dee5df698c0ab24e20967d
Poncholay <guillaume.wilmot@epitech.eu>
2019-03-03 23:14:59 +0100

    Gateway redirect headers and body

--- api/gateway/src/controllers/GatewayController.kt
+++ api/gateway/src/controllers/GatewayController.kt
@@ -11,8 +11,10 @@ import io.ktor.client.response.readText
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
+import io.ktor.request.receiveText
 import io.ktor.response.respond
 import io.ktor.routing.*
+import io.ktor.util.filter
 import me.guillaumewilmot.api.gateway.config.MicroserviceConfig
 import me.guillaumewilmot.api.gateway.models.responses.ResponseModel
 import me.guillaumewilmot.api.gateway.util.to
@@ -35,9 +37,18 @@ object GatewayController {
                     val client = HttpClient(Apache) {
                         expectSuccess = false
                     }
+                    val callHeaders = call.request.headers.filter { header, _ ->
+                        arrayOf(
+                                "content-length",
+                                "content-type"
+                        ).find { header.equals(it, ignoreCase = true) } == null
+                    }
                     val response = client.request<HttpResponse> {
                         url(URL("http://localhost:$port/$param"))
                         method = httpMethod
+                        headers.clear()
+                        headers.appendAll(callHeaders)
+                        body = call.receiveText()
                     }
                     val content = response.readText().to<ResponseModel>()
                     if (content != null) {
